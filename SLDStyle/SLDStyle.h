6fc66a0471792ca53633726be775e0739db209d8
Luis Rosas <lrosasarce@gmail.com>
2024-03-29 15:25:54 -0500

    First commit

---
+++ SLDStyle/SLDStyle.h
@@ -0,0 +1,18 @@
+//
+//  SLDStyle.h
+//  SLDStyle
+//
+//  Created by Luis Alberto Rosas Arce on 29/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SLDStyle.
+FOUNDATION_EXPORT double SLDStyleVersionNumber;
+
+//! Project version string for SLDStyle.
+FOUNDATION_EXPORT const unsigned char SLDStyleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLDStyle/PublicHeader.h>
+
+
---
+++ SLDStyleDemo/SLDStyleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SLDStyleDemoApp.swift
+//  SLDStyleDemo
+//
+//  Created by Luis Alberto Rosas Arce on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SLDStyleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
